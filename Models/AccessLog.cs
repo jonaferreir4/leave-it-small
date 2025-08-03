999b5b4909e34cfd583cbbbb4e686e6d0aeccc20
jonaferreir4 <jonaferreira2204@gmail.com>
2025-08-03 19:04:06 -0300

    feat: create AccessLog Model

---
+++ Models/AccessLog.cs
@@ -0,0 +1,11 @@
+namespace leave_it_small.Models;
+
+public class AccessLog
+{
+    public Guid Id { get; set; }
+    public Guid ShortenedUrlId { get; set; }
+    public string IpAdress { get; set; } = string.Empty;
+    public string UserAgent { get; set; } = string.Empty;
+    public DateTime AccessDate { get; set; }
+
+}
